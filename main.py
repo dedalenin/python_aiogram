e355111d30d2ae28f7dcbdcd5aca0f221916e978
Dedalenin <mikhailliashenko@yandex.ru>
2025-06-15 19:10:24 +0300

    echo bot smart

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
-BOT_TOKEN = '7631068222:AAGun7-RCWP21wPCO1uMSoSROvSQVd9ezuE'
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+BOT_TOKEN = 'BOT TOKEN HERE'
 
 # Создаем объекты бота и диспетчера
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token='7631068222:AAGun7-RCWP21wPCO1uMSoSROvSQVd9ezuE')
 dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -25,12 +24,33 @@ async def process_help_command(message: Message):
     )
 
 
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
+
+async def send_voice_message(message: Message):
+    await message.repy_voice(message.voice.file_id)
+
+async def send_audio_message(message:Message):
+    await message.reply_audio(message.audio.file_id)
+
+async def send_sticker_message(message:Message) -> None:
+    await message.reply_sticker(message.sticker.file_id)
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_voice_message, F.voice)
+dp.message.register(send_audio_message, F.audio)
+dp.message.register(send_sticker_message, F.sticker)
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
